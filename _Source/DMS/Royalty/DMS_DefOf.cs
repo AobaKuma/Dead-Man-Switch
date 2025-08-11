8f87141f8a99a983fe4f9494f52ab4a5ae207015
Aoba <sakilomo@gmail.com>
2025-08-12 05:00:54 +0800

    更新
    - 給所有零件加了介紹連結
    - 增加了殖民艦隊送穿梭機的皇權權限。
    - 壁虎現在可以安裝額外的改裝模塊了

--- _Source/DMS/Royalty/DMS_DefOf.cs
+++ _Source/DMS/Royalty/DMS_DefOf.cs
@@ -6,6 +6,10 @@ namespace DMS
     [DefOf]
     internal static class DMS_DefOf
     {
+        static DMS_DefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(DMS_DefOf));
+        }
         public static FactionDef DMS_Army;
         public static QuestScriptDef DMS_PromotionCeremony;
         public static PawnKindDef DMS_Officer_Ceremonist;
